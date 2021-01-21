f6950782821e343bf5d4dab5729eba1a61831b86
seemam <seema_meghre@hotmail.com>
2021-01-21 11:38:25 -0500

    Moved const to env

--- backend/utils/send-message.js
+++ backend/utils/send-message.js
@@ -1,13 +1,18 @@
 const Nexmo = require("nexmo")
 
+require("dotenv").config()
+
+const apiKey = process.env.VONAGE_API_KEY
+const apiSecret = process.env.VONAGE_API_SECRET
+
 function sendMessage(text){
     const nexmo = new Nexmo({
-        apiKey: '5222cc63',
-        apiSecret: '0YZisImNOTL2nizq',
+        apiKey: apiKey,
+        apiSecret: apiSecret,
       });
       
-      const from = '15877604428';
-      const to = '16478813554';
+      const from = process.env.FROM;
+      const to = process.env.TO;
       // const text = 'Hello from Vonage SMS API';
 
     // nexmo.message.sendSms(from, to, text, {}, (error, response) => {
